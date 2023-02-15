21e0c933ac9c1c49b5b70ebcb0bcd431746586df
gmero <g.merola95@gmail.com>
2023-02-15 16:55:40 +0100

    Divisione ModelBuilding - ValidationSet - TestSet

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ __category__ = ["Action", "Adventure", "Animation", "Comedy", "Crime", "Document
                 "History", "Horror", "Music", "Mystery", "Romance", "Science Fiction", "TV Movie", "Thriller", "War",
                 "Western"]
 # Variabili globali
-USER_N = 610
+USER_N = 50
 FILM_N = 9719
 
 def generate_like_matrix(urm, movie, a,dislike=False):
@@ -103,6 +103,24 @@ def toNL(Matrix,Nnibor):
     NL = pd.DataFrame(v[:, -Nnibor:]).rename(columns=lambda x: f'Max{x + 1}')
     return NL
 
+def generate_prediction(urm,UNL,SimMatrix):
+    PredictionMatrix = pd.DataFrame(columns=range(1, 193610), index=range(1, USER_N + 1))
+    for au in range(0, USER_N):
+        if (urm.index == au).any():
+            mean_rating_au = urm.loc[au + 1].mean()
+        else:
+            mean_rating_au = 0
+        for movie in urm.columns[0:]:
+            if not math.isnan(urm.loc[au + 1][movie]):
+                # print("UTENTE: "+str(au))
+                # print("FILM: "+str(movie))
+                PredictionMatrix.iloc[au][movie] = predict(SimMatrix, au, UNL[au], movie, urm, mean_rating_au)
+            else:
+                PredictionMatrix.iloc[au][movie] = 7000
+        # Elimino colonne NaN (i movieId non sono contigui)
+    PredictionMatrix = PredictionMatrix.dropna(axis=1, how='all')
+    return PredictionMatrix
+
 
 # Funzione per la predizione e il riempimento della urm (FUNZIONANTE (ritorna voti da 0 a 5) )
 def predict(SimMatrix, au, SetAu, movie, urm, meanRateAu):
@@ -140,20 +158,32 @@ def generate_UNL(SNL, PNL,Nnibor):
     return UNL
 
 
+def mae(urm,PredictionMatrix):
+    num_of_entry_err = 0
+    sum_of_entry_error = 0
+    for u in range(1, USER_N + 1):
+        for movie in urm.columns[0:]:
+            r_predicted = PredictionMatrix.loc[u][movie]
+            r_real = urm.loc[u][movie]
+            if not math.isnan(r_predicted) and not math.isnan(r_real) and r_predicted != 7000:
+                entry_error = abs(r_predicted - r_real)
+                sum_of_entry_error += entry_error
+                num_of_entry_err += 1
+    return sum_of_entry_error / num_of_entry_err
+
+#MODEL BUILIDING
+
 # IMPORT FILE CSV
 ratings = pd.read_csv('ml-latest-small/ratings.csv')
-movieList = pd.read_csv('ml-latest-small/movies.csv', nrows=7001)
-
+movieList = pd.read_csv('ml-latest-small/movies.csv', nrows=5001)
 colnames = ['movieId', 'title', 'genres']
-test_movie = pd.read_csv('ml-latest-small/movies.csv', skiprows=7001, nrows=2741, names=colnames, header=None)
-review_film_test = ratings.merge(test_movie, on='movieId', how='inner')
-# print(review_film_test.head(1000).sort_values(by=["userId"]))
-urm_test = review_film_test.pivot_table(index='userId', columns='movieId', values='rating')
+validation_movie = pd.read_csv('ml-latest-small/movies.csv', skiprows=5001, nrows=2000, names=colnames, header=None)
+review_film_validation = ratings.merge(validation_movie, on='movieId', how='inner')
+urm_validation = review_film_validation.pivot_table(index='userId', columns='movieId', values='rating')
 # Se l'utente non ha film nell'urm_test allora aggiungo l'utente con una riga di NaN
 for i in range(1, 611):
-    if not (urm_test.index == i).any():
-        urm_test.loc[i] = float("nan")
-#print(urm_test)
+    if not (urm_validation.index == i).any():
+        urm_validation.loc[i] = float("nan")
 
 review_film = ratings.merge(movieList, on='movieId', how='inner')
 
@@ -161,6 +191,9 @@ review_film = ratings.merge(movieList, on='movieId', how='inner')
 urm = review_film.pivot_table(index='userId', columns='movieId', values='rating')
 print("GENERATE LE URM")
 
+mina=0
+minnn=0
+minmae=0
 for a in range(4,7):
     a=a/2
     for Nnibor in range(10,41,10):
@@ -174,7 +207,6 @@ for a in range(4,7):
         SemSimMatrix = pd.DataFrame(columns=range(1, USER_N + 1), index=range(1, USER_N + 1))
         PearsonSimMatrix = pd.DataFrame(columns=range(1, USER_N + 1), index=range(1, USER_N + 1))
         JaccardSimMatrix = pd.DataFrame(columns=range(1, USER_N + 1), index=range(1, USER_N + 1))
-        PredictionMatrix = pd.DataFrame(columns=range(1, 193610), index=range(1, USER_N + 1))
 
         # Calolo SemSim, SimPearson, SimJaccard
         for i in range(1, USER_N + 1):
@@ -214,46 +246,103 @@ for a in range(4,7):
         UNL = generate_UNL(SNL, PNL,Nnibor)
         print("CALCOLATA LA UNL")
         # Calcolo dei ratings predetti
-        for au in range(0, USER_N):
-            if (urm_test.index == au).any():
-                mean_rating_au = urm_test.loc[au+1].mean()
-            else:
-                mean_rating_au = 0
-            for movie in urm_test.columns[0:]:
-                if not math.isnan(urm_test.loc[au+1][movie]):
-                    #print("UTENTE: "+str(au))
-                    #print("FILM: "+str(movie))
-                    PredictionMatrix.iloc[au][movie] = predict(SimMatrix, au, UNL[au], movie, urm_test, mean_rating_au)
-                else:
-                    PredictionMatrix.iloc[au][movie] = 7000
-            # Elimino colonne NaN (i movieId non sono contigui)
-            PredictionMatrix = PredictionMatrix.dropna(axis=1, how='all')
-        #print(PredictionMatrix)
-
+        PredictionMatrix= generate_prediction(urm_validation,UNL,SimMatrix)
         print("CALCOLATA LA PREDICTION MATRIX")
+        mean_absolute_error= mae(urm_validation, PredictionMatrix)
 
-        num_of_entry_err = 0
-        sum_of_entry_error = 0
-        for u in range(1,USER_N+1):
-            for movie in urm_test.columns[0:]:
-                r_predicted = PredictionMatrix.loc[u][movie]
-                r_real = urm_test.loc[u][movie]
-                if not math.isnan(r_predicted) and not math.isnan(r_real) and r_predicted != 7000:
-                    entry_error = abs(r_predicted - r_real)
-                    #print("Calcolato Entry error per user["+(str(u))+"] per l'item "+str(movie)+": "+str(entry_error))
-                    sum_of_entry_error += entry_error
-                    num_of_entry_err += 1
-                    #print("NUMERO DI ERRORE: "+str(num_of_entry_err))
-        MAE = sum_of_entry_error / num_of_entry_err
+        #MODEL SELECTION
+        if minmae>mean_absolute_error:
+            minmae=mean_absolute_error
+            mina=a
+            minnn=Nnibor
+
+        #ANNOTAZIONE RISULTATI
         f=open("result.txt","a")
-        time=time.time() - start_time
-        f.write("[Time= "+str(time)+"] [a= "+str(a)+"] [ NUM_NIBOR= "+str(Nnibor)+"] La MAE E': "+str(MAE))+"\n"
+        timeF=time.time() - start_time
+        del start_time
+        f.write("[Time= "+str(timeF)+"] [a= "+str(a)+"] [ NUM_NIBOR= "+str(Nnibor)+"] La MAE E': "+str(mean_absolute_error)+"\n")
         f.close()
-        print("LA MAE E': ")
-        print(MAE)
+
         del PredictionMatrix
         del SimMatrix
         del PreSimMatrix
 
 
 
+#ADDESTRAMENTO MODELLO MIGLIORE SUL TRAINING COMPLETO
+del movieList
+movieList = pd.read_csv('ml-latest-small/movies.csv', nrows=7001)
+colnames = ['movieId', 'title', 'genres']
+test_movie = pd.read_csv('ml-latest-small/movies.csv', skiprows=7001, nrows=2741, names=colnames, header=None)
+review_film_test = ratings.merge(test_movie, on='movieId', how='inner')
+urm_test = review_film_test.pivot_table(index='userId', columns='movieId', values='rating')
+# Se l'utente non ha film nell'urm_test allora aggiungo l'utente con una riga di NaN
+for i in range(1, 611):
+    if not (urm_test.index == i).any():
+        urm_test.loc[i] = float("nan")
+del review_film
+del urm
+review_film = ratings.merge(movieList, on='movieId', how='inner')
+# USER RATING MATRIX
+urm = review_film.pivot_table(index='userId', columns='movieId', values='rating')
+
+start_time = time.time()
+#Generazione LikeSet e DisLikeSet
+LikeSet = generate_like_matrix(urm,movieList,mina)
+DisLikeSet = generate_like_matrix(urm, movieList, mina, dislike=True)
+
+print("GENERATI I LIKE SET")
+# Allocazione matrici
+SemSimMatrix = pd.DataFrame(columns=range(1, USER_N + 1), index=range(1, USER_N + 1))
+PearsonSimMatrix = pd.DataFrame(columns=range(1, USER_N + 1), index=range(1, USER_N + 1))
+JaccardSimMatrix = pd.DataFrame(columns=range(1, USER_N + 1), index=range(1, USER_N + 1))
+
+# Calolo SemSim, SimPearson, SimJaccard
+for i in range(1, USER_N + 1):
+    for j in range(1, USER_N + 1):
+        # SemSim
+        SemSimPlus = sem_sim(LikeSet[i], LikeSet[j])
+        SemSimMinus = sem_sim(DisLikeSet[i], DisLikeSet[j])
+        SemSim = (SemSimPlus + SemSimMinus) / 2
+        # Per ogni coppia di utenti (i,j), calcolo il suo valore di similarità con le tre funzioni
+        SemSimMatrix.iloc[i - 1][j] = SemSim
+        # Non ci interessa sapere la somiglianza con lo stesso utente, inseriamo un valore placeholder per riempire la matrice
+        if i == j:
+            SemSimMatrix.iloc[i - 1][j] = -1
+        # Pearson
+        SimPearson = pearson(i, j)
+        PearsonSimMatrix.iloc[i - 1][j] = SimPearson
+        if i == j:
+            PearsonSimMatrix.iloc[i - 1][j] = -5
+        # Jaccard
+        SimJaccard = jaccard(i, j)
+        JaccardSimMatrix.iloc[i - 1][j] = SimJaccard
+        if i == j:
+            JaccardSimMatrix.iloc[i - 1][j] = 1
+
+# Calcolo PreSimMatrix e SimMatrix
+PreSimMatrix = JaccardSimMatrix * PearsonSimMatrix
+SimMatrix = (PreSimMatrix + SemSimMatrix) / 2
+
+print("CALCOLATA LA SIMMATRIX")
+# Recupero i NUM_NIBOR elementi massimi di SemSimMatrix e PreSimMatrix
+# SNL (Semantic Neighborhood List)
+# PNL (PreSim Neighborhood List
+SNL = toNL(SemSimMatrix,minnn)
+PNL = toNL(PreSimMatrix,minnn)
+
+# UNL (Unified Neighborhood List) : Intersezione di SNL e PNL
+UNL = generate_UNL(SNL, PNL,minnn)
+print("CALCOLATA LA UNL")
+# Calcolo dei ratings predetti
+PredictionMatrix= generate_prediction(urm_test,UNL,SimMatrix)
+print("CALCOLATA LA PREDICTION MATRIX")
+
+#RISULTATO TEST
+mean_absolute_error= mae(urm_test, PredictionMatrix)
+
+#ANNOTAZIONE RISULTATO FINALE
+f=open("result.txt","a")
+time=time.time() - start_time
+f.write("\n\nRISULTATO FINALE: [Time= "+str(time)+"] [a= "+str(mina)+"] [ NUM_NIBOR= "+str(minnn)+"] La MAE E': "+str(mean_absolute_error)+"\n")
+f.close()
